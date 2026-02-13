4ff553fb93f07552a99c501cdcc6c0dd33648a7a
joao <joaobpedro@gmail.com>
2026-02-13 09:53:38 +0100

    config changes 2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
+-- ~/.config/nvim/lua/config/options.lua
+local opt = vim.opt
+
+-- Set global indentation to 4 spaces
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true -- Use spaces instead of tabs
